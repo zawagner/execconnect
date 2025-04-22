9b0a06665be5fc6a16edfc9fd6af307bb714fed0
zawagner <zawagner@gmail.com>
2025-04-22 13:47:09 -0400

    Delete saas-landingpage-react@2.0.0 directory

--- saas-landingpage-react@2.0.0/prettier.config.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-    printWidth: 120,
-    tabWidth: 4,
-    semi: true,
-    bracketSpacing: true,
-    arrowParens: "always",
-    bracketSameLine: true,
-    plugins: ["prettier-plugin-tailwindcss"],
-};
